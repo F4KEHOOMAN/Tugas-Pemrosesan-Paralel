155afcb1f00a20cecd085d27410443c295dedf3f
Khalifaturahman Al Hazri Repaldi <46299092+F4KEHOOMAN@users.noreply.github.com>
2020-11-20 09:43:46 +0700

    Monitoring Code in VM(Tugas 1)

--- monitoring.py
+++ monitoring.py
@@ -10,16 +10,3 @@ print(f"Memori : {psutil.virtual_memory().percent}%")
 tx = psutil.net_io_counters(pernic=True)['enp0s3'].bytes_sent
 rx = psutil.net_io_counters(pernic=True)['enp0s3'].bytes_recv
 print(f"Tx/Rx : {tx}kbps / {rx}kbps")
-
-'''
-jaringan = psutil.net_io_counters(pernic=False)
-tx = jaringan[2] / 1000
-rx = jaringan[3] / 1000
-
-time.sleep(2)
-'''
-    
-
-    
-
-    
